a5f18ebe972c882bcda94f4cc1f67cee06fe46a0
Alperen Bozkurt <bozkurttalperen@gmail.com>
2018-03-17 01:24:04 +0300

    Birkaç test yazıldı. Close #7

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,17 +2,26 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get users_index_url
+    get root_path
     assert_response :success
   end
 
-  test "should get show" do
-    get users_show_url
-    assert_response :success
+  test "if counter successfully increasing" do      # Sayacın doğru bir şekilde artıp artmadığını kontrol eden test.
+    get user_show_path("alperen")                   # user_show_path 'e bir username yollayarak sayacı arttırıyorum
+    get user_show_path("alperen")                   # Veritanbanında kayıtlı bir kullanıcı olduğu için sayac burada 3 olacak. (bkz: test/fixtures/user.yml)
+    user = User.find_by(username: "alperen")        # Veritabanından son durumu getiriyorum.
+    assert user.count == 3                          # Eğer olması gereken sayı ile veritabanı eşitse test başarılı.
+  end
+
+  test "if the user not exists" do                  # Olmayan bir kullanıcıya istek yollanırsa veritabanına eklenmemesi gerekiyor.
+    get user_show_path("wefjhwef")                  # Olmayan bir kullanıcıya istek yapılıyor.
+    user = User.find_by(username: "wefjhwef")       # Veritabanından çekmeye çalışıyoruz.
+
+    assert user.nil?                                #Eğer veritabanına eklemezse user nil olacak. Eğer user nilse test başarılıdır.
   end
 
   test "should get most" do
-    get users_most_url
+    get most_popular_users_path
     assert_response :success
   end
 
