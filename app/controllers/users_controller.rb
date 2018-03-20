eff1f59f62efe17288319c6f43cad28ac77d6332
Alperen Bozkurt <bozkurttalperen@gmail.com>
2018-03-20 15:10:34 +0300

    Satır sayısındaki gecikme iyileştirildi. Close #17

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
+  layout false, :only => [:line]
   def index
     @user = User.new
   end
 
+  def line
+    @git_user = get_user_info(params[:username])
+    @line_count = get_line_count(params[:username], @git_user["repos"])
+  end
+
   def show
+
     @user = User.find_by(username: params[:username])               # Eğer kullanıcı var ise getirdik
     @git_user = get_user_info(params[:username])                    # Githubdan kullanıcının verilerini getirdik.
 
@@ -42,28 +49,15 @@ class UsersController < ApplicationController
     user  = JSON.parse(response_user.body)        # Gelen stringi daha rahat kullanmak için hash'e çevirdik
     repos = JSON.parse(response_repos.body)
 
-    line_sum = 0
-
-    repos.each do |repo|
-      langs = connection.get("repos/#{ username }/#{ repo['name'] }/languages?access_token=#{access_token}")
-      langs = JSON.parse(langs.body)      # Bu iki satırda hangi dillerle kaç satır yazıldığının bilgisi geliyor.
-      line_sum += langs.values.sum        # gelen veriler önce birbiriyle, sonra da diğer depoların satır sayıları ile toplanıyor.
-
-
-
-    end
-
 
     stars = sum_star(repos)
     fav_lang = fav_lang(repos)
 
-    line_sum  = { "line_counter" => spaces_on(line_sum) }  # spaces_on metodu alt tarafta tanımlı.
     repos     = { "repos" => repos }                          # user değişkeni ile birleştirmek için hash haline getiriyoruz.
     fav_lang  = { "fav_lang" => fav_lang }
     stars     = { "stars" => stars }
 
     user.merge! repos         # repoları user değişkeninin içerisine atarak tek bir değişken return ediyoruz.
-    user.merge! line_sum
     user.merge! fav_lang
     user.merge! stars
   end
@@ -90,5 +84,20 @@ class UsersController < ApplicationController
     end
 
     langs.max_by{ |lang| langs.count(lang) }     # En çok tekrar eden dili return ediyorum.
+  end
+
+  def get_line_count(username, repos)
+    access_token = "8cb02b324d0713ad491bd957c708f274be313719"
+    cert = File.join(File.dirname(__FILE__), "../../app/assets/certs/cacert.pem")
+    connection = Faraday.new('https://api.github.com', ssl: { ca_file: cert })
+
+    line_sum = 0
+
+    repos.each do |repo|
+      langs = connection.get("repos/#{ username }/#{ repo['name'] }/languages?access_token=#{access_token}")
+      langs = JSON.parse(langs.body)      # Bu iki satırda hangi dillerle kaç satır yazıldığının bilgisi geliyor.
+      line_sum += langs.values.sum        # gelen veriler önce birbiriyle, sonra da diğer depoların satır sayıları ile toplanıyor.
+    end
+    spaces_on(line_sum)                   # Yukarıda tanımlı line_sum methodu ile sayı daha okunaklı yapıldı.
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root  'users#index'
-  get   '/most'       , to: 'users#most',   as: 'most_popular_users'
-  get   '/:username'  , to: 'users#show',   as: 'user_show'
-  post  '/search'     , to: 'users#search', as: 'user_search'
+  get   '/most'             , to: 'users#most',   as: 'most_popular_users'
+  get   '/:username'        , to: 'users#show',   as: 'user_show'
+  get   '/:username/line'   , to: 'users#line',   as: 'user_line'
+  post  '/search'           , to: 'users#search', as: 'user_search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
