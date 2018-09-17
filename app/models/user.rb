e044e4889a3ad312d968baf4bbeb03d3ebd77330
Alperen Bozkurt <alperen.bozkurt@adeosecurity.com>
2018-09-17 12:20:38 +0300

    Annotate gem ile açıklamalar eklendi.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint(8)        not null, primary key
+#  username   :string
+#  count      :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  image_url  :string
+#
+
 class User < ApplicationRecord
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint(8)        not null, primary key
+#  username   :string
+#  count      :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  image_url  :string
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
